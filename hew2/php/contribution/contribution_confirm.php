8428a213b45422c79e8764acc38b2616073f2d0e
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-12 18:49:07 +0900

    nanan

--- hew2/php/contribution/contribution_confirm.php
+++ hew2/php/contribution/contribution_confirm.php
@@ -1,13 +1,26 @@
 <?php
-require_once '../tpl/contribution_confirm.php';
+session_start();
+
+$memo = '';
+$memo_range = '';
+
+$range = $_SESSION['range'];
+$category = $_SESSION['category'];
+$text1 = $_SESSION['text1'];
+$text2 = $_SESSION['text2'];
+$radio = $_SESSION['radio'];
+if (isset($_SESSION['memo_range'])) {
+  $memo_range = $_SESSION['memo_range'];
+}
+if (isset($_SESSION['memo'])) {
+  $memo = $_SESSION['memo'];
+}
+else{
+  $memo = 'なし';
+}
 
 
 
-
-
-
-
-
-
+require_once '../../tpl/contribution/contribution_confirm.php';
 
  ?>
--- hew2/php/contribution/contribution_form.php
+++ hew2/php/contribution/contribution_form.php
@@ -1,13 +1,55 @@
 <?php
-require_once '../tpl/contribution_form.php';
+session_start();
+$text_err = '';
+$range_err = '';
+$radio_error = '' ;
+$category_err = '';
+if (isset($_POST['next'])) {
+  $error_flag = '';//エラーフラグ
+  if ($_POST['category'] == 'default') {//カテゴリのエラーチェック
+    $category_err = 'カテゴリーが未選択です。';
+    $error_flag = 'error';
+  }
+  else{//セッションに保存
+    $_SESSION['category'] = $_POST['category'];
+  }
+  if ($_POST['range']=='default') {//公開範囲エラーチェック
+    $error_flag = 'error';
+    $range_err = '公開範囲が未選択です。';
 
+  }
+  else{
+    $_SESSION['range']=$_POST['range'];
+  }
+  if($_POST['text1'] == '' || $_POST['text2'] == '') {//テキストのエラーチェック
+    $error_flag = 'error';
+    $text_err = 'テキストが未記入です。';
+  }
+  else{
+    $_SESSION['text1'] = $_POST['text1'];
+    $_SESSION['text2'] = $_POST['text2'];
+  }
+  if (empty($_POST['radio'])) {//理由の部分のエラーチェック
+    $error_flag = 'error';
+    $radio_error = '理由を選択してください。';
+  }
+  else{
+    $_SESSION['radio'] = $_POST['radio'];
+  }
 
+  if ($_POST['memo_range'] != '') {//メモの公開範囲のをセッションに保存
+    $_SESSION['memo_range'] = $_POST['memo_range'];
+  }
 
+  if ($_POST['memo'] != '' ) {//メモをセッションに保存
+    $_SESSION['memo'] = $_POST['memo'];
+  }
 
+  if ($error_flag == '') {
+    header('Location:contribution_confirm.php');
+  }
 
+}
 
-
-
-
-
+require_once '../../tpl/contribution/contribution_form.php';
  ?>
