de12ff71da71937de81ccdd06ddf26c0c4b762bd
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-15 11:19:47 +0900

    update tpl_account

--- hew2/tpl/account/account_register_provisional_success.php
+++ hew2/tpl/account/account_register_provisional_success.php
@@ -2,14 +2,15 @@
 <html lang="ja">
 	<head>
 		<meta charset="UTF-8">
-		<title>TOP</title>
+		<title>仮登録完了</title>
 
 	</head>
 
 <body>
 <div class="container">
-<h1>TOP</h1>
-<a href="php/login.php">ログイン</a>
+<h1>仮登録完了</h1>
+<p>メールを送信致しましたのでメールを確認してください</p>
+<p><a href="#" onClick="window.close(); return false;"target="_blank">ページを閉じる</a></p>
 </div>
 </body>
 </html>
