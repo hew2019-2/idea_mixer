9a4ba6a931d7f6818fe175be9b0478d13e3408cc
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-15 11:18:40 +0900

    add account file

--- hew2/php/account/account_register_provisional_confirm.php
+++ hew2/php/account/account_register_provisional_confirm.php
@@ -1,6 +1,11 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 require_once '../../config.php';
 require_once '../../class/user.php';
+require '../../PHPMailer/src/Exception.php';
+require '../../PHPMailer/src/PHPMailer.php';
+require '../../PHPMailer/src/SMTP.php';
 session_start();
 
 // DBに登録し、complete画面に遷移
@@ -11,17 +16,86 @@ if (!empty($_POST['submit'])) {
         $pdo = new PDO ('mysql:dbname='.DB.';host='.DB_HOST.';charset=utf8', DB_USER, DB_PASS);
 
         // ハッシュ化
-
+		$hash_login_id = $users->get_login_id();
+		$hash_password = $users->get_password();
+		$stretch = rand(1000,10000);		//ストレッチ回数
+		$salt = uniqid();			//ソルト値
+		for($i=0;$i<$stretch;$i++){			//ハッシュ化
+			$hash_login_id = md5($salt.$hash_login_id);
+			$hash_password = md5($salt.$hash_password);
+		}
+		//日付取得
+		$updated_at = date('Y-m-d H:i:s');
+		$created_at = $updated_at;
         // SQL作成
-        $sql = "INSERT INTO users (name, email, login_id,
+        $sql = "INSERT INTO users (name, email, login_id,hash_login_id,
                                     hash_password, salt, stretch,
-                                    update_at, created_at, status)
+                                    updated_at, created_at, status)
                 VALUES ('".$users->get_name()."', '".$users->get_email()."', '".$users->get_login_id()."',
-                        '".$hash_password."', '".$salt."', '".$stretch."',
-                        '".$update_at."', '".$created_at."', '".$status."')";
+                         '".$hash_login_id."','".$hash_password."', '".$salt."', '".$stretch."',
+                        '".$updated_at."', '".$created_at."',0)";
 
         // SQL実行(DB登録)
         $pdo->query($sql);
+
+		//メール送信
+		$mail = new PHPMailer(true);
+
+		try {
+		  //Gmail 認証情報
+		  $host = 'smtp.gmail.com';
+		  $username = 'HAL2018iw02@gmail.com'; // 送信元gmailアドレス
+		  $password = 'HAL2018iw02';  //gmailアカウントのパスワード
+
+		  //差出人
+		  $from = 'HAL_HEW@gmail.com';	//
+		  $fromname = 'HAL';	//
+
+		  //宛先
+		  $to = $users->get_email();
+		  $toname = $users->get_name();
+
+		  //件名・本文
+		  $subject = '仮登録ありがとうございます';
+		  $body = 	'-----------------------------------------------------------'."\r\n".
+		  			'もしも本メールにお心あたりがない場合は、そのまま本メールを破棄頂きますよう'."\r\n".
+					'お願い申し上げます。'."\r\n".
+					'-----------------------------------------------------------'."\r\n".
+					$toname.'様'."\r\n".
+		  			'本登録にお進みの場合は以下のURLをクリックし本登録にお進みください'."\r\n".
+		  			'http://localhost/hew2/php/account/account_register_provisional_success.php?id='.$hash_login_id;
+
+		  //メール設定
+		  $mail->SMTPDebug = 2; //デバッグ用
+		  $mail->isSMTP();
+		  $mail->SMTPAuth = true;
+		  $mail->Host = $host;
+		  $mail->Username = $username;
+		  $mail->Password = $password;
+		  $mail->SMTPSecure = 'tls';
+		  $mail->Port = 587;
+		  $mail->CharSet = "utf-8";
+		  $mail->Encoding = "base64";
+		  $mail->setFrom($from, $fromname);
+		  $mail->addAddress($to, $toname);
+		  $mail->Subject = $subject;
+		  $mail->Body    = $body;
+
+		  //メール送信
+		  $mail->send();
+		  echo '成功';
+
+		} catch (Exception $e) {
+		  echo '失敗: ', $mail->ErrorInfo;
+		}
+
+
+
+
+
+
+
+
 
     } catch (PDOException $e) {
         // エラー出力
--- hew2/php/account/account_register_provisional_success.php
+++ hew2/php/account/account_register_provisional_success.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../tpl/register_success.php';
+require_once '../../tpl/account/account_register_provisional_success.php';
 
 
 
