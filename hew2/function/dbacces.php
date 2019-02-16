3602c11a7c7da70b805e0c52d0febf1f363cb13e
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-16 13:38:00 +0900

    Add files via upload

---
+++ hew2/function/dbacces.php
@@ -0,0 +1,9 @@
+<?php 
+function database(){
+    $dsn ='mysql:dbname='.DB.';host='.DB_HOST.';charset=utf8';
+    $user = DB_USER;
+    $password = DB_PASS;
+    $dbh = new PDO($dsn,$user,$password);
+    return $dbh;
+};
+ ?>
