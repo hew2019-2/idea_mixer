1c431b17aae2a6a7a42ed84e0bf5467f98df5547
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-16 09:57:50 +0900

    update const

--- hew2/config.php
+++ hew2/config.php
@@ -1,7 +1,7 @@
 <?php
-const DB = 'hew_2019';
+const DB = 'hew_2019_idea_mixer';
 const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASS = 'root';
+const DB_USER = 'hew1902_admin';
+const DB_PASS = 'admin1234';
 const FROM = '';
 const BASE_URL = '';
