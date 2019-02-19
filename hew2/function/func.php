899903bb70f9c8e23ca2df42e74e800b239196bf
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-19 10:07:49 +0900

    Update func.php

--- hew2/function/func.php
+++ hew2/function/func.php
@@ -3,3 +3,13 @@
     function h($value) {
         return htmlspecialchars($value);
     }
+
+	// PASS IDハッシュ化
+	//引数1:ソルト 引数2:ストレッチ 引数3:ハッシュ化する文字列
+	//戻り値:ハッシュ化後の文字列
+	function hash_value($salt,$stretch,$str){
+		for($i=0;$i<$stretch;$i++){			//ハッシュ化
+			$hash = md5($salt.$str);
+		}
+		return $hash;
+	}
