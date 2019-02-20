127d1c961c58bfde1f033e7b4924b2dd41920b6f
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-20 17:30:36 +0900

    updated contribution

--- hew2/tpl/contribution/contribution_form.php
+++ hew2/tpl/contribution/contribution_form.php
@@ -1,105 +1,107 @@
-<?php
-require_once '../../config.php';
-require_once '../../function/dbacces.php';
-$dbh = database();
-session_start();
-$_SESSION['user_id'] = 2;
-$user_id = 2;
-$text_err = '';
-$range_err = '';
-$radio_error = '' ;
-$category_err = '';
-$m_c_error = '';
-$rows['keyword'] = '';
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>投稿確認</title>
+	<script type="text/javascript" src="../../js/jquery-2.0.2.min.js"></script>
+	<style>
+		.display{display: hidden;}
+	</style>
+</head>
+<body>
+<div class="container">
+<h1>TOP</h1>
+<h2>投稿</h2>
+<form action="contribution_form.php" method="post">
+	<!--
+		投句のカテゴリー選択
+	-->
+	<p><?php echo $m_c_error ?></p>
+	<?php echo $category_err//error表示 ?>
+	<?php echo $range_err ?>
+	<?php echo $text_err ?>
+	<?php echo $radio_error ?>
+	<select class='' name="category">
+		<option value=0>カテゴリを選択しください</option>
+		<?php
+			$i = 1;
+			foreach ($category_array as $value) {
+				echo '<option value="'.$i.'">'.$value.'</option>';
+				$i++;
+			}
+		?>
+	</select>
+<!--
+	公開範囲を選択
+ -->
+	<div class='box1'>
+		<select name='range'>
+			<option value=0>公開範囲</option>
+			<option value=1>全体に公開</option>
+			<!--デバック用php作成出来たら消して下さい-->
+			<option value=2>チーム内に公開</option>
+			<!-- ↑↑↑ここ↑↑↑ -->
+			<?php foreach ($team as $team): ?>
+				<option value='<?php echo $team?>'><?php echo $team ?></option><!--$team_numの初期値０をphp側で設定して下さい-->
+			<?php endforeach; ?>
+			<option value=3>自分だけに公開</option>
+		</select>
+	</div>
+<!--
+	アイデアの元とするワードの入力
+ -->
+	<div class='box2'>
+		<input type='text' name='text1'>
+			×
+		<input type='text' name='text2' id='word'>
+		<button type='button' name='random' id='random_word' onclick='click_event()'>ランダム</button>
+	</div>
 
-$sql = "SELECT name FROM categories";
-$stmt = $dbh->query($sql);
-$category_array = array();
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {    // SELECT~LIMITクエリで取得したレコード全てをfetch
-    $category_array[] = $row['name'];   // fetchした結果をレコード単位で配列$dispに保存
-}
-$dbh = null;
+	<div class='box3'>
+		<h3>※必ず一つの項目を選択、又は記入して下さい</h3>
+		<h2>理由</h2>
+		<input type='radio' name='radio' value=1>内容が面白そうだったから
+		<input type='radio' name='radio' value=2>内容が斬新だったから
+		<input type='radio' name='radio' value=3>なんか”ぴん”ときたから
+		<input type='radio' name='radio' value=4>新しいものが作れそうだから
+		<input type='radio' name='radio' value=5 id='other'>その他
+		<div class='display'>
+			<textarea name='other' rows="3" cols="30"></textarea>
+		</div>
+	</div>
 
+	<div class='box4'>
+		<h2>メモ</h2><p>※任意でご記入ください</p>
+		<select name='memo_range'>
+			<option value=0>公開範囲</option>
+			<option value='1'>全体に公開</option>
+			<option value='2'>ティーム内に公開</option><!--デバック用php作成出来たら消して下さい-->
+			<?php foreach ($team as $team): ?>
+				<option value='2'><?php echo $team ?></option><!--$team_numの初期値０をphp側で設定して下さい-->
+			<?php endforeach; ?>
+			<option value='3'>自分だけに公開</option>
+		</select>
+		<textarea name='memo' rows='4' cols='40'></textarea>
+	</div>
 
-if (isset($_POST['next'])) {
-//
-//エラーフラグ
-//
-  $error_flag = '';
-//
-//カテゴリのエラーチェック
-//
-  if ($_POST['category'] == 0) {
-   $category_err = 'カテゴリーが未選択です。';
-   $error_flag = 'error';
-  }
-  else{
-    $_SESSION['category'] = $_POST['category'];
-  }
-//
-//公開範囲エラーチェック＆セッションに保存
-//
-  if ($_POST['range']==0) {
-    $error_flag = 'error';
-    $range_err = '公開範囲が未選択です。';
-  }
-  else{
-    $_SESSION['range']=$_POST['range'];
-  }
-  //初期ページの初期化
+	<input type='submit' name='next' value='確認画面へ'>
 
-//ランダムボタンが押された時
-
-//
-//テキストのエラーチェック＆セッションに保存
-//
-  if($_POST['text1'] == '' || $_POST['text2'] == '') {
-    $error_flag = 'error';
-    $text_err = 'テキストが未記入です。';
-  }
-  else{
-    $_SESSION['text1'] = $_POST['text1'];
-    $_SESSION['text2'] = $_POST['text2'];
-  }
-//
-//理由の部分のエラーチェック&セッションに保存
-//
-  if (empty($_POST['radio'])) {
-    $error_flag = 'error';
-    $radio_error = '理由を選択してください。';
-  }
-  else{
-    $_SESSION['radio'] = $_POST['radio'];
-    $_SESSION['other'] = $_POST['other'];
-  }
-//
-//メモの公開範囲をセッションに保存
-//
-  if ($_POST['memo_range'] != '') {
-    $_SESSION['memo_range'] = $_POST['memo_range'];
-  }
-//
-//メモをセッションに保存
-//
-  if ($_POST['memo'] != '' ) {
-    $_SESSION['memo'] = $_POST['memo'];
-  }
-//
-//公開範囲とメモの範囲の設定がおかしかったらエラー
-//
-if ($_POST['range'] > $_POST['memo_range']) {
-   $m_c_error = 'カテゴリとメモのレンジの設定が正しくありません';
-   $error_flag = 'error';
-}
-//
-//エラーがなかった場合に次のページへ移動する
-//
-  if ($error_flag == '') {
-    header('Location:contribution_confirm.php');
-    exit();
-  }
-
-}
-
-require_once '../../tpl/contribution/contribution_form.php';
- ?>
+</form>
+</div>
+<script type="text/javascript">
+	// document.getElementById('random_word').onclick=function(){
+	// document.getElementById('word').value='';
+	//MAXのカラム数を取得
+	function click_event(){
+		$.ajax({
+	        url: "../../php/contribution/dbaccess.php",
+	        type: "POST",
+	        success: function(response) {
+	            // console.log("ajax通信に成功しました");
+				document.getElementById("word").value = response;
+	        }
+	    });
+	}
+</script>
+</body>
+</html>
--- hew2/tpl/contribution/contribution_view.php
+++ hew2/tpl/contribution/contribution_view.php
@@ -1,97 +1,61 @@
-<?php
-   require_once '../../config.php';
-   require_once '../../function/dbacces.php';
-   session_start();
-   $dbh = database();
-   $dbh -> SetAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
-   $idea_id = 1;
-   $account_id = 1;
-//
-//アイデア取得時の初期化
-//
-   $word1 = '';
-   $word2 = '';
-   $date = '';
-   $reason = '';
-   $memo = '';
-   $idea_sharerange = '';
-   $memo_sharerange = '';
-   $update = '';
-//
-//アイデア取得
-//
-   $ideas = $dbh->query("SELECT user_id , keyword_id_1 , keyword_id_2 , category_id , reason_id , reason_otherwise , memo , range_sharewith_idea_switch , range_sharewith_memo_switch , created_at , updated_at FROM ideas WHERE '".$idea_id."' ");
-   while ($row = $ideas->fetch(PDO::FETCH_ASSOC)){
-     $user_id = $row['user_id'];
-     $word1 = $row['keyword_id_1'];
-     $word2 = $row['keyword_id_2'];
-     $category_id = $row['category_id'];
-     $reason_id = $row['reason_id'];
-     $reason_otherwise = $row['reason_otherwise'];
-     $memo = $row['memo'];
-     $idea_sharerange = $row['range_sharewith_idea_switch'];
-     $memo_sharerange = $row['range_sharewith_memo_switch'];
-     $date = $row['created_at'];
-     $update = $row['updated_at'];
-   }
-//カテゴリ取得時の初期化
-   $account_name = '';
-   $category = '';
-//
-//カテゴリの取得
-//
-   $category = $dbh->query("SELECT category FROM categories WHERE '".$category_id."' ");
-   while ($row = $category->fetch(PDO::FETCH_ASSOC)){
-     $category_name = $row['category'];
-   }
-   $name = '';
-//アカウントの名前を取得
-   $account = $dbh->query("SELECT name FROM users WHERE '".$account_id."' ");
-   while ($row = $account->fetch(PDO::FETCH_ASSOC)){
-     $name = $row['name'];
-   }
-   //
-   //リプライがあった際保存するプログラム
-   //
-   if (!empty($_POST['reply_btn'])) {
-      // $reply_user = $_SESSION['user_id'];
-      $reply_user = 21;
-      $reply_comment = $_POST['reply'];
-      $reply = $dbh->prepare("INSERT INTO comments(idea_id,user_id,comment) VALUES('".$idea_id."','".$reply_user."','".$reply_comment."')");
-      $reply->execute();
+ <!DOCTYPE html>
+ <html lang="ja" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>投稿表示</title>
+   </head>
+   <body>
+     <h1>投稿表示</h1>
+     <div class='header'>
+       <p class='logo'>TOP</p>
+     </div>
+     <form action="contribution_view.php" method="post">
 
-      header('Location:contribution_view.php');
-      exit;
-   }
-//コメントの初期化
-   $comments = array();
-//
-//コメントとユーザーネーム取得
-//
-   $comments_sql = $dbh->query("SELECT comments.comment, users.name FROM comments LEFT OUTER JOIN users ON comments.user_id = users.id  WHERE '".$idea_id."'");
-   while ($row = $comments_sql->fetch(PDO::FETCH_ASSOC)){
-      $comment = $row['comment'];
-      $comment_name = $row['name'];
+     <div class='account'>
+       <img src='account_image'>
+       <p><?php $account_name ?></p>
+       <?php //↓↓↓ここから↓↓↓ ?>
+       <?php echo $button ?>
+       <?php //↑↑↑ここまでをphpで処理する↑↑↑ ?>
+     </div>
+ <!-- ワードを表示 -->
+     <div class='contents'>
+       <p>カテゴリー：<?php echo $category_name //データベースからカテゴリを抽出する ?></p>
+       <a href='#?word=<?php echo $word1 ?>'><?php echo $word1 //データベースからワード１を抽出する ?></a>
+       <p>×</p>
+       <a href='#?word=<?php echo $word2 ?>'><?php echo $word2 //データベースからワード２を抽出する ?></a>
+       <p><?php $date ?></p>
+     </div>
+ <!-- 理由を表示 -->
+     <div class='reason'>
+       <dl class='reason_con'>
+         <dt>理由：</dt>
+         <dd><?php echo $reason //データベースから理由を抽出する ?></dd>
+       </dl>
+     </div>
+     </hr>
+ <!-- メモ表示 -->
+     <div class='memo'>
+       <dl class='memo_con'>
+         <dt>メモ：</dt>
+         <dd><?php echo $memo //データベースからメモを抽出 ?></dd>
+       </dl>
+     </div>
 
-      $comments[] = array('comment'=>$comment,'name'=>$comment_name);
-   }
-//
-//表示した際のフォローボタンか編集ボタンかを変える処理
-//
-   if ($account_id == $user_id) {
-     $button = "<button type='submit' name='edit' class='account_btn'>編集</button>";
-   }
-   else{
-     $button = "<button type='submit' name='follow' class='account_btn'>フォロー</button>";
-   }
-//
-//followがきた時の処理
-//
+     <div class='reply'>
+       <input type='text' name='reply'>
+       <input type='submit' name='reply_btn' value='コメント'>
+     </div>
 
+     <div class='coments'>
+       <dl>
+         <dt>コメント</dt>
+       <?php foreach ($comments as list('comment'=>$comment,'name'=>$name) ): ?>
+         <dd> <?php echo $name ?> <img src="#"><?php echo $comment ?></dd>
+       <?php endforeach; ?>
+     </dl>
+     </div>
 
-//
-//編集を押された時の処理
-//
-
-
-require_once '../../tpl/contribution/contribution_view.php';
+   </form>
+   </body>
+ </html>
