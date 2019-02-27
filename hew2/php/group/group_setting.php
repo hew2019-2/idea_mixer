11e1dd38b4c7b7534637a0b13c4f5f23e1fb7e88
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-27 15:32:38 +0900

    add files

--- hew2/php/group/group_setting.php
+++ hew2/php/group/group_setting.php
@@ -1,13 +1,15 @@
-<?php
-require_once '../tpl/group_setting.php';
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>TOP</title>
 
+	</head>
 
-
-
-
-
-
-
-
-
- ?>
+<body>
+<div class="container">
+<h1>TOP</h1>
+<a href="php/login.php">ログイン</a>
+</div>
+</body>
+</html>
