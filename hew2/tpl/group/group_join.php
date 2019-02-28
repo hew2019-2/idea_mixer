bf834e5ebb23dfdd7da20ef37246ad988c44d1df
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-28 20:46:28 +0900

    group_joinを追加

---
+++ hew2/tpl/group/group_join.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>グループに参加</title>
+	</head>
+<body>
+<div class="container">
+<h1>グループに参加しました</h1>
+<span>グループ名：<?php echo $group['id'];?></span>
+</div>
+<a href="../index.php"></a>
+</body>
+</html>
