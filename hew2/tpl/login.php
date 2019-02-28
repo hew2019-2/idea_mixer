f38b713eb5e3b812a5b2c9ca31b0f6f0b7cd01b5
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-02-28 20:32:07 +0900

    ログインできるようにした

--- hew2/tpl/login.php
+++ hew2/tpl/login.php
@@ -2,14 +2,20 @@
 <html lang="ja">
 	<head>
 		<meta charset="UTF-8">
-		<title>TOP</title>
+		<title>ログイン</title>
 
 	</head>
 
 <body>
 <div class="container">
-<h1>TOP</h1>
-<a href="login.php">ログイン</a>
+<h1>ログイン</h1>
+<form action="login.php" method="post">
+ログインID<input type="text" name="login_id" value="<?=$prof['login_id']?>"><span><?php echo $err_msg['login_id'];?></span>
+<br />
+パスワード<input type="password" name="login_password"><span><?php echo $err_msg['login_password'];?></span>
+<br />
+<button type="submit" name="status" value="login">ログイン</button>
+</form>
 </div>
 </body>
 </html>
