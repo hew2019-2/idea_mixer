1cf46808d6e2985e2ece80c5d413e54e751b7b74
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-03-01 13:50:02 +0900

    update group_setting

--- hew2/tpl/group/group_setting.php
+++ hew2/tpl/group/group_setting.php
@@ -2,15 +2,28 @@
 <html lang="ja">
 	<head>
 		<meta charset="UTF-8">
-		<title>TOP</title>
-
+		<title>グループ設定</title>
 	</head>
-
 <body>
 <div class="container">
-<h1>TOP</h1>
-<label>グループ名:</label>
-<a href="php/login.php">ログイン</a>
+<h1>グループ設定</h1>
+<form action="group_setting_complete.php" method="post">
+	-----------------------------<br />
+	グループ名:<input type="text" name="name" value="<?php echo $group['name'];?>">
+	<br />
+	-----------------------------<br />
+	メンバー：
+	<?php echo $member[0]['name']; array_shift($member);?>(リーダー)
+	<?php foreach ($member as $user) { ?>
+		<br />
+		<input type="checkbox" value="<?php echo $user['id'];?>"><?php echo $user['name'];?>
+	<?php }?>
+	<br />
+	-----------------------------
+	<br />
+	<button type="submit" name="submit" value="setting">確認</button>
+</form>
+<a href="php/group/group_top.php">グループTOPに戻る</a>
 </div>
 </body>
 </html>
---
+++ hew2/tpl/group/group_setting_complete.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>グループ設定/完了</title>
+	</head>
+<body>
+</body>
+</html>
