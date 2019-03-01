6a67e3a86e89674553219dbd77cd4c6d50203bf8
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-03-01 16:17:00 +0900

    Add files via upload

--- hew2/php/index.php
+++ hew2/php/index.php
@@ -1,13 +1,19 @@
 <?php
-require_once '../tpl/index.php';
+  require_once '../config.php';
 
+  $dsn = 'mysql:dbname='.DB.';host='.DB_HOST.';charset=utf8';
+  $dbuser = DB_USER;
+  $dbpass = DB_PASS;
+  
+  $dbh= new PDO($dsn,$dbuser,$dbpass);
+  $sql = "SELECT i.id idea_id , user_id, k1.keyword keyword1 , k2.keyword keyword2 FROM ideas i INNER JOIN keywords k1 ON i.keyword_id_1 = k1.id INNER JOIN keywords k2 ON i.keyword_id_2 = k2.id";
+  $stmt = $dbh -> query($sql);
 
+  $ideas = array();
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $ideas[] = $row;
+  }
+  $dbh = null;
 
-
-
-
-
-
-
-
- ?>
+  require_once '../tpl/index.php';
+?>
