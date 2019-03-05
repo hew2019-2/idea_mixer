73c7097fb8a7bbac7ec0214858a1fda9382ff3df
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-03-05 16:44:17 +0900

    Add files via upload

--- hew2/php/index.php
+++ hew2/php/index.php
@@ -3,7 +3,7 @@
   require_once '../function/dbacces.php';
 
   $dbh = database();
-  $sql = "SELECT i.id, keyword_id_1, keyword_id_2, c.id, category FROM ideas i LEFT OUTER JOIN categories c ON i.category_id = c.id ";
+  $sql = "SELECT i.id idea_id, keyword_id_1, keyword_id_2, c.id category_id, category FROM ideas i INNER JOIN categories c ON i.category_id = c.id ";
   $stmt = $dbh->query($sql);
 
   $ideas = array();
