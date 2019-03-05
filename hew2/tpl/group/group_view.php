57df87d31ca29309f1dc706ff89ef0c66b10add3
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-03-05 10:32:34 +0900

    グループビュー編集

--- hew2/tpl/group/group_view.php
+++ hew2/tpl/group/group_view.php
@@ -1,12 +1,22 @@
-<?php
-
-
-
-
-
-
-
-
-
-
- ?>
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>グループ一覧</title>
+	</head>
+<body>
+<div class="container">
+<table>
+	<tr>
+		<td></td><td>グループ名</td><td></td><td></td>
+	</tr>
+	<?php foreach($result as $val){ ?>
+	<tr>
+		<td></td><td><?php echo $val['name'] ?></td><td><?php echo $val['name'] ?></td>
+	</tr>
+	<?php } ?>
+</table>
+<a href="php/group/group_top.php">グループTOPに戻る</a>
+</div>
+</body>
+</html>
