0e70a1b1c3eeb30bfa9928d6853dcd98fa91275c
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-03-05 10:22:11 +0900

    Add files via upload

--- hew2/index.php
+++ hew2/index.php
@@ -1,35 +1,3 @@
 <?php
-  require_once '../config.php';
-  session_start();
-  /*
-  session_start();
-  if(!empty($_SESSION['login'])){
-    $user = array();
-    $user['login'] = $_SESSION['login'];
-  }
-  else{
-    header('Location:./index.php');
-  }*/
-
-  $dsn = 'mysql:dbname='.DB.';host='.DB_HOST.';charset=utf8';
-  $dbuser = DB_USER;
-  $dbpass = DB_PASS;
-  
-  $dbh= new PDO($dsn,$dbuser,$dbpass);
-  $sql = "SELECT i.id , user_id, k1.keyword keyword1 , k2.keyword keyword2 FROM ideas i INNER JOIN keywords k1 ON i.keyword_id_1 = k1.id INNER JOIN keywords k2 ON i.keyword_id_2 = k2.id";
-  $stmt = $dbh -> query($sql);
-
-  $ideas = array();
-  while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    $ideas[] = $row;
-  }
-  $dbh = null;
-
-  var_dump($ideas);
-
-  
-  $user['user_id'] = $ideas['user_id'];
-  $_SESSION['login'] = $user;
-
-  require_once '../tpl/index.php';
+  header('Location:php/index.php');
 ?>
