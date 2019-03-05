5580b79e1a8e122b5cf9745f4123dea06e1b20d7
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-03-05 10:22:46 +0900

    Add files via upload

--- hew2/php/index.php
+++ hew2/php/index.php
@@ -1,13 +1,10 @@
 <?php
   require_once '../config.php';
+  require_once '../function/dbacces.php';
 
-  $dsn = 'mysql:dbname='.DB.';host='.DB_HOST.';charset=utf8';
-  $dbuser = DB_USER;
-  $dbpass = DB_PASS;
-  
-  $dbh= new PDO($dsn,$dbuser,$dbpass);
-  $sql = "SELECT i.id idea_id , user_id, k1.keyword keyword1 , k2.keyword keyword2 FROM ideas i INNER JOIN keywords k1 ON i.keyword_id_1 = k1.id INNER JOIN keywords k2 ON i.keyword_id_2 = k2.id";
-  $stmt = $dbh -> query($sql);
+  $dbh = database();
+  $sql = "SELECT i.id, keyword_id_1, keyword_id_2, c.id, category FROM ideas i LEFT OUTER JOIN categories c ON i.category_id = c.id ";
+  $stmt = $dbh->query($sql);
 
   $ideas = array();
   while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
