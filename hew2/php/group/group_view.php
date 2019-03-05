23cf87420d48590f59fbfd7383594bd52cb84262
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-03-05 10:31:46 +0900

    グループビュー編集

--- hew2/php/group/group_view.php
+++ hew2/php/group/group_view.php
@@ -1,15 +1,26 @@
-<!DOCTYPE html>
-<html lang="ja">
-	<head>
-		<meta charset="UTF-8">
-		<title>TOP</title>
+<?php
+require_once '../../function/dbacces.php';
+require_once '../../config.php';
+session_start();
+$_SESSION['login'] = 2;
+if(isset($_SESSION['login'])){
+	$dbh = database();
+	$sql = "SELECT * FROM groups_tagmaps
+			INNER JOIN groups ON groups_tagmaps.group_id = groups.id
+			INNER JOIN ideas ON groups.id = ideas.group_id
+			WHERE groups_tagmaps.user_id = ".$_SESSION['login'];
+	$stmt = $dbh->query($sql);
+	$result = $stmt -> fetchall(PDO::FETCH_ASSOC);
+}
+else{
+	header('location:../../index.php');
+}
 
-	</head>
 
-<body>
-<div class="container">
-<h1>TOP</h1>
-<a href="php/login.php">ログイン</a>
-</div>
-</body>
-</html>
+
+
+
+
+
+require_once '../../tpl/group/group_view.php';
+?>
