5b9dc80e629db7cddbd9d95fa388cddf2d943a05
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-03-08 17:56:36 +0900

    Add files via upload

---
+++ hew2/tpl/footer.php
@@ -0,0 +1,21 @@
+<!-- Footer -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-FF5722 fixed-bottom">
+      <div class="container">
+        <div class="col-3">
+          <a id="home" class="nav-link" href="./index.php">ホーム</a>
+        </div>
+        <div class="col-3">
+          <a id="post" class="nav-link" href="./contribution/contribution_form.php">投稿</a>
+        </div>
+        <div class="col-3">
+          <a id="mypage" class="nav-link" href="./mypage.php">マイページ</a>
+        </div>
+        <div class="col-3">
+          <a id="group" class="nav-link" href="./group/group_view.php">グループ</a>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
