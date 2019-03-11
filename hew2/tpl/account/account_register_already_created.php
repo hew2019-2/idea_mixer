1be2634416d4f19994ddb5b46bcce5bfbe5788e4
hew2019-2 <47557536+hew2019-2@users.noreply.github.com>
2019-03-11 09:24:49 +0900

    Add files via upload

--- hew2/tpl/account/account_register_already_created.php
+++ hew2/tpl/account/account_register_already_created.php
@@ -1,9 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>既に登録</title>
+		<link href="../../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../../css/account.css" rel="stylesheet" type="text/css">
+	</head>
+
+<body>
+	<?php require_once "../../tpl/choose_header.php"?>
+
+	<div class="container">
+	<p>お客様のIDはすでに本登録済みなのでログインページよりログインしてください</p>
+	<a href="../login.php">ログインページへ</a>
+	</div>
 
 
+<?php require_once "../../tpl/footer.php"?>
+<!-- Bootstrap core JavaScript -->
+<script src="../../js/jquery-3.3.1.min.js"></script>
+<script src="../../js/bootstrap.bundle.min.js"></script>
 
-
-
-
-require_once '../../tpl/account/account_register_already_created.php';
- ?>
+</body>
+</html>
